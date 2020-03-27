d241a42e1ab37dd1dc62754d9e091b96cc14f2e3
1027046y <61856113+1027046y@users.noreply.github.com>
2020-03-27 12:04:11 +0800

    完成椅子移動

---
+++ VR/Assets/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("燈光群組")]
+    public GameObject lightgroup;
+    [Header("會動的椅子")]
+    public Transform chest;
+    public  IEnumerator Effectlight()
+    {
+        lightgroup.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
+        lightgroup.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        lightgroup.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        lightgroup.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        lightgroup.SetActive(false);
+        yield return new WaitForSeconds(0.15f);
+        lightgroup.SetActive(true);
+    }
+   public void StartMoveChest()
+    {
+        StartCoroutine(MoveChest());
+    }
+    public IEnumerator MoveChest()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            chest.position -= chest.forward * 0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        chest.GetComponent<CapsuleCollider>().enabled = false;
+
+
+
+    }
+    private void Start()
+    {
+        StartCoroutine(Effectlight());
+    }
+}
